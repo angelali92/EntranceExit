79ca4ea169aca31af7d1d1f757d117d25d56fd1c
angelali92 <angela@li>
2015-05-19 23:00:11 -0700

    changed settings for development

--- entranceexit/wsgi.py
+++ entranceexit/wsgi.py
@@ -16,7 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entranceexit.settings")
 application = get_wsgi_application()
 
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
